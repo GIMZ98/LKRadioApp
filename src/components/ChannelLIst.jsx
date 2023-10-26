dd12a7e76b0ef2661429a4bf24dc065920d8f41f
gimz98 <udnamod98@gmail.com>
2023-10-26 20:07:17 +0530

    Testing for one channel

--- src/components/ChannelLIst.jsx
+++ src/components/ChannelLIst.jsx
@@ -5,11 +5,14 @@ import { Howl } from 'howler';
 const ChannelLIst = () => {
   const [playing, setPlaying] = useState(false)
   const [event, setEvent] = useState(undefined)
+  const [sound, setSound] = useState()
 
 
 
-  var sound;
   const playClick = (_event) => {
+    // if(sound!=undefined){
+    //     sound.pause()
+    // }
     setEvent(_event);
 
     if($(_event.target).text()=='▶️'){
@@ -19,20 +22,22 @@ const ChannelLIst = () => {
 
     if($(_event.target).text()=='⏹️'){
         setPlaying(false)
+        if(sound!=undefined){
+          sound.pause()
+        }
         // $(event.target).text('')
         $(event.target).text('▶️')
         console.log("pause")
     }
     //$(event.target).text('⏹️')
-    console.log("SLBC clicked")
-    sound = new Howl({
-        src: [],
+    //console.log("SLBC clicked")
+    const sound_ = new Howl({
+        src: [import.meta.env.VITE_SLBC_URI],
         html5: true,
     });
-    console.log('sound', sound)
-    console.log('event', event.target.className)
-
-   //sound.play() 
+    //console.log('sound', sound_)
+    //console.log('event', _event.target.className)
+    setSound(sound_)
   }
 
   const stopClick = async () => {
@@ -42,9 +47,23 @@ const ChannelLIst = () => {
   }
 
   useEffect(() => {
+    if(sound!=undefined){
+        if(playing){
+            sound.play()
+            console.log("start")
+        }
+        if(!playing){
+            sound.pause()
+            console.log("paused")
+        }
+    }
+
+  }, [sound])
+
+  useEffect(() => {
     if(playing && (event!=undefined)){
         $('button').text('▶️')
-        console.log('useEffect got event: ', event)
+        //console.log('useEffect got event: ', event)
         $(event.target).text('⏹️')
     }
   }, [event])
@@ -54,35 +73,30 @@ const ChannelLIst = () => {
     <>
       <div className='flex flex-wrap items-center justify-center max-w-screen min-h-screen bg-blue-500 m-[20px]'>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='SITHA' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='HIRU' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='LAKVIRU' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='RAN' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='SIRASA' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='SRI' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
         <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
+            <button onClick={playClick} id='YFM' className='px-[2px] py-[2px] bg-slate-0 text-2xl'>▶️</button>
         </div>
-        <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
-        </div>
-        <div className='flex items-center justify-center sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-blue-600 border-1 border-white m-[5px]'>
-            <button onClick={playClick} id='SLBC' className='px-[2px] py-[2px] bg-black text-2xl'>▶️</button>
-        </div>
+        
        
       </div>
 
